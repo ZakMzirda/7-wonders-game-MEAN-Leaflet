0257d500aacee30dea863293b5023581e47b5520
MZIRDA Zakaria <zakaria.mzirda@outlook.fr>
2018-02-01 22:44:27 +0100

    algo distance

--- public/app/controllers/mapCtrl.js
+++ public/app/controllers/mapCtrl.js
@@ -1,10 +1,12 @@
 angular.module('mapControllers', [])
 .controller('leafletCtrl',function($scope, $http){
 	var mymap = L.map('mapid').setView([48.8583701, 2.2922926, 17], 13);
-	var coordx = 48.8583701;
-	var coordy = 2.2922926;
+	
 	$scope.ville = "";
 	$scope.question=[];
+	$scope.crdx=[];
+	$scope.crdy=[];
+
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 8,
 		id: 'mapbox.streets'
@@ -15,6 +17,8 @@ angular.module('mapControllers', [])
 	/*Avoir les coordonnées x et y*/ 
 		
 	var popup = L.popup();
+
+	//fonction pour avoir le nom de ville avec un click
 	function onMapClick(e) {
 		popup
 			.setLatLng(e.latlng)
@@ -23,35 +27,61 @@ angular.module('mapControllers', [])
 		$http.get("https://nominatim.openstreetmap.org/reverse?format=json&lat="+e.latlng.lat+"&lon="+e.latlng.lng)
 		.success(function (response) {
 			$scope.ville=response.address.town;
-			console.log($scope.ville);
+			//console.log($scope.ville);
 		})	
 	}
-
-	function CalculDistance(e){
-		var score = 100;
-		var distance = Math.sqrt(((coordx-e.latlng.lat)*(coordx-e.latlng.lat))+((coordy-e.latlng.lng)*(coordy-e.latlng.lng)));
-		//console.log('Distance : '+ distance);
-		//requete pour recuperer les données json
+	this.getrndQuestion=function Getrndquestion(){
 		$http.get('api/GetQuestion').success(function (response) {
+			
 			var taille = response.maquestion.length;
+			var randomvalue = Math.floor(Math.random() * taille)
 			for(var i =0; i<taille; i++){
 				$scope.question[i]=response.maquestion[i].laquestion;
-				//console.log($scope.question[i]);
-			}
-			//console.log($scope.question)
-			
-		})	
+				$scope.crdx[i]=response.maquestion[i].coordonnee_x;
+				$scope.crdy[i]=response.maquestion[i].coordonnee_y;
 
-		if(distance <= 0.1){
-			console.log('Bien joué! votre score est '+ score);
-		}
-		if(distance >= 0.1){
-			score=score-80;
-			console.log('Vous êtes loin! votre score est '+score);
-		}
+			}
+
+			
+			var randomQuestion = $scope.question[randomvalue];
+			var randomQuestion_crdx = $scope.crdx[randomvalue];
+			var randomQuestion_crdy = $scope.crdy[randomvalue];
+			
+			console.log(randomQuestion);
+			//console.log(randomQuestion_crdx);
+			//console.log(randomQuestion_crdy);
+
+			function CalculDistance(e){
+				var score = 100;
+				var distance = Math.sqrt(((randomQuestion_crdx-e.latlng.lat)*(randomQuestion_crdx-e.latlng.lat))+((randomQuestion_crdy-e.latlng.lng)*(randomQuestion_crdy-e.latlng.lng)));
+				console.log('Distance : '+ distance);
+				//requete pour recuperer les données json
+				
+				if(distance <= 0.4){
+					console.log('Bien joué! votre score est '+ score);
+				}
+				if(distance >= 0.44 && distance<=0.66){
+					score=score-20;
+					console.log('Vous êtes proche! votre score est '+score);
+				}
+				if(distance >= 0.67 && distance<=0.8){
+					score=score-50;
+					console.log('Vous êtes loin! votre score est '+score);
+				}
+				if(distance>0.8){
+					score=score-100;
+					console.log('Oulala trop loin! votre score est '+score);
+				}
+				
+			}
+			mymap.on('click', CalculDistance);
+
+		})
+		
 	}
 	
-	//mymap.on('click', onMapClick);
-	mymap.on('click', CalculDistance);
+	
+	mymap.on('click', onMapClick);
+	
 
 });
