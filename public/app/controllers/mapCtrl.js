3f84093d6bacce2479839bb4a5a633a91ddb8b48
MZIRDA Zakaria <zakaria.mzirda@outlook.fr>
2018-02-02 23:30:43 +0100

    on peut jouer maintenant (modif mapCtrl & home)

--- public/app/controllers/mapCtrl.js
+++ public/app/controllers/mapCtrl.js
@@ -1,36 +1,63 @@
 angular.module('mapControllers', [])
 .controller('leafletCtrl',function($scope, $http){
-	var mymap = L.map('mapid').setView([48.8583701, 2.2922926, 17], 13);
+	var mymap = L.map('mapid').setView([0, 0, 0], 2);
 	
-	$scope.ville = "";
+	//$scope.ville = "";
 	$scope.question=[];
 	$scope.crdx=[];
 	$scope.crdy=[];
+	var info =this;//pour pouvoir recuperer les donnes a l'interieur des fonctions et les utiliser dans les pages html
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 8,
+		maxZoom: 18,
+		minZoom: 2,
 		id: 'mapbox.streets'
 	}).addTo(mymap);
-	L.marker([48.8583701, 2.2922926, 17]).addTo(mymap)
-		.bindPopup("La tour eiffel").openPopup();
-	
-	/*Avoir les coordonnées x et y*/ 
-		
-	var popup = L.popup();
 
-	//fonction pour avoir le nom de ville avec un click
+	//icon
+	var redIcon = L.icon({
+		iconUrl: 'outils/images/redmarker.png',
+		iconSize:     [20, 30], // size of the icon
+		
+	});
+
+	//markers
+	L.marker([48.8583701, 2.2922926], {icon: redIcon}).addTo(mymap)
+		.bindPopup("La tour eiffel").openPopup();
+	L.marker([48.8583701, 2.2922926], {icon: redIcon}).addTo(mymap);
+
+	//rome
+	L.marker([41.8902102, 12.4900422], {icon: redIcon}).addTo(mymap);
+	//chine
+	L.marker([40.4319077, 116.5681862], {icon: redIcon}).addTo(mymap);
+	//petra
+	L.marker([30.3284544, 35.4421735], {icon: redIcon}).addTo(mymap);
+	//Le Christ Rédempteur à Rio de Janeiro
+	L.marker([-22.951916, -43.2126759], {icon: redIcon}).addTo(mymap);
+	//Machu Picchu 
+	L.marker([-13.1631412, -72.5471516], {icon: redIcon}).addTo(mymap);
+	//Chichen Itza
+	L.marker([20.6842849, -88.5699713], {icon: redIcon}).addTo(mymap);
+	//Le Taj Mahal
+	L.marker([27.1750151, 78.0399665], {icon: redIcon}).addTo(mymap);
+
+	//fonction pour avoir le nom de ville avec un click & les coordonnées de l'endroit
+	var popup = L.popup();
 	function onMapClick(e) {
 		popup
 			.setLatLng(e.latlng)
 			.setContent("Les coordonnées de cet endroit " + e.latlng.toString())
 			.openOn(mymap);
-		$http.get("https://nominatim.openstreetmap.org/reverse?format=json&lat="+e.latlng.lat+"&lon="+e.latlng.lng)
+		/*$http.get("https://nominatim.openstreetmap.org/reverse?format=json&lat="+e.latlng.lat+"&lon="+e.latlng.lng)
 		.success(function (response) {
 			$scope.ville=response.address.town;
 			//console.log($scope.ville);
-		})	
+		})	*/
 	}
+	
+	info.nbrofclick = 0;
 	this.getrndQuestion=function Getrndquestion(){
+		info.rndquestion="";
 		$http.get('api/GetQuestion').success(function (response) {
 			
 			var taille = response.maquestion.length;
@@ -47,39 +74,70 @@ angular.module('mapControllers', [])
 			var randomQuestion_crdx = $scope.crdx[randomvalue];
 			var randomQuestion_crdy = $scope.crdy[randomvalue];
 			
-			console.log(randomQuestion);
+			
+			//console.log(randomQuestion);
 			//console.log(randomQuestion_crdx);
 			//console.log(randomQuestion_crdy);
-
 			function CalculDistance(e){
 				var score = 100;
-				var distance = Math.sqrt(((randomQuestion_crdx-e.latlng.lat)*(randomQuestion_crdx-e.latlng.lat))+((randomQuestion_crdy-e.latlng.lng)*(randomQuestion_crdy-e.latlng.lng)));
+				var distance = Math.sqrt(((randomQuestion_crdx-e.latlng.lat)*(randomQuestion_crdx-e.latlng.lat))
+				+((randomQuestion_crdy-e.latlng.lng)*(randomQuestion_crdy-e.latlng.lng)));
 				console.log('Distance : '+ distance);
 				//requete pour recuperer les données json
 				
 				if(distance <= 0.4){
-					console.log('Bien joué! votre score est '+ score);
+					
+					info.chance='Excellent!! votre score est '+ score +' sur 100';
+					console.log(info.chance);
 				}
-				if(distance >= 0.44 && distance<=0.66){
-					score=score-20;
-					console.log('Vous êtes proche! votre score est '+score);
+				if(distance > 0.4 && distance<=0.5){
+					score=score-5;
+					info.chance='Bien! votre score est '+ score +' sur 100, presque parfait!';
 				}
-				if(distance >= 0.67 && distance<=0.8){
-					score=score-50;
-					console.log('Vous êtes loin! votre score est '+score);
+				if(distance > 0.5 && distance<=0.6){
+					score=score-10;
+					
+					info.chance='Vous étiez proche! votre score est '+score+' sur 100, vous pouvez faire mieux';
 				}
-				if(distance>0.8){
+				if(distance>0.6 && distance <= 0.7){
+					score=score-30;
+					
+					info.chance='Vous êtes un peu loin là! votre score est '+score +' sur 100';					
+				}
+				if(distance>0.7 && distance <= 0.9){
+					score=score-80;
+					
+					info.chance='Vous êtes loin! votre score est '+score +' sur 100';					
+				}
+				if(distance>0.9){
 					score=score-100;
-					console.log('Oulala trop loin! votre score est '+score);
+					
+					info.chance='Oulala trop loin!! votre score est '+score +' sur 100';					
 				}
-				
 			}
-			mymap.on('click', CalculDistance);
+			
+			info.rndquestion=randomQuestion;
+			
+			//console.log(info.nbrofclick);
 
+			if(info.nbrofclick>2){
+				info.chance=null;
+				info.nbrofclick=1;
+			}
+
+
+			mymap.on('click', CalculDistance);
+			
 		})
-		
+		info.nbrofclick++;
+
+		mymap.on('click', Getrndquestion);
 	}
-	
+	this.resetquestion=function resetQuestion(){
+		info.chance=null;
+		info.randomQuestion=null;
+		info.nbrofclick=0;
+	}
 	
 	mymap.on('click', onMapClick);
 	
