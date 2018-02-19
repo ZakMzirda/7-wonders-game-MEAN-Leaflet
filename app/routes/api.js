e87a0f94a13910b3536adce7136240fe85a40013
Abdoulaye Diallo <abdoul653@gmail.com>
2018-02-19 14:24:47 +0100

    commit

--- app/routes/api.js
+++ app/routes/api.js
@@ -5,9 +5,9 @@ module.exports = function(router) {
     //creation d'une route http://localhost:8080/users on peut tester le fonctionnement de notre route avec postman  
     router.post('/questions', function(req, res) {
         var question = new Question(); // Create new User object
-        question.laquestion = req.body.laquestion; // Save username from request to User object
-        question.coordonnee_x = req.body.coordonnee_x; // Save password from request to User object
-        question.coordonnee_y = req.body.coordonnee_y; // Save email from request to User object
+        question.laquestion = req.body.laquestion; 
+        question.coordonnee_x = req.body.coordonnee_x; 
+        question.coordonnee_y = req.body.coordonnee_y; 
         question.les_scores=0;
         
             if(req.body.laquestion==null || req.body.laquestion=='' || req.body.coordonnee_x==null || req.body.coordonnee_x=='' || req.body.coordonnee_y==null || req.body.coordonnee_y==''){
