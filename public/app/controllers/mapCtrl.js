6c5df85df9603b77e930dee82f48dffa131e277b
MZIRDA Zakaria <zakaria.mzirda@outlook.fr>
2018-02-21 21:21:29 +0100

    changement front end (essai sur une question plusieurs fois)

--- public/app/controllers/mapCtrl.js
+++ public/app/controllers/mapCtrl.js
@@ -4,10 +4,12 @@ angular.module('mapControllers', [])
 	
 	//$scope.ville = "";
 	$scope.question=[];
+	$scope.randomvalue=0;
 	$scope.crdx=[];
 	$scope.crdy=[];
 	$scope.ids=[];
 	$scope.ApiCall='';
+	$scope.essais=0;
 	$scope.donnee=null;
 	var info =this;//pour pouvoir recuperer les donnes a l'interieur des fonctions et les utiliser dans les pages html
 
@@ -32,25 +34,27 @@ angular.module('mapControllers', [])
 
 
 	//markers
-	L.marker([48.8583701, 2.2922926], {icon: redIcon}).addTo(mymap)
+	/*L.marker([48.8583701, 2.2922926], {icon: redIcon}).addTo(mymap)
 		.bindPopup("La tour eiffel").openPopup();
-	L.marker([48.8583701, 2.2922926], {icon: redIcon}).addTo(mymap);
+	L.marker([48.8583701, 2.2922926], {icon: redIcon}).addTo(mymap);*/
 
-	//rome
-	L.marker([41.8902102, 12.4900422], {icon: redIcon}).addTo(mymap);
-	//chine
-	L.marker([40.4319077, 116.5681862], {icon: redIcon}).addTo(mymap);
-	//petra
-	L.marker([30.3284544, 35.4421735], {icon: redIcon}).addTo(mymap);
-	//Le Christ Rédempteur à Rio de Janeiro
-	L.marker([-22.951916, -43.2126759], {icon: redIcon}).addTo(mymap);
-	//Machu Picchu 
-	L.marker([-13.1631412, -72.5471516], {icon: redIcon}).addTo(mymap);
-	//Chichen Itza
-	L.marker([20.6842849, -88.5699713], {icon: redIcon}).addTo(mymap);
-	//Le Taj Mahal
-	L.marker([27.1750151, 78.0399665], {icon: redIcon}).addTo(mymap);
+	//merveilles du monde
+	//la pyramide de Khéops
+	L.marker([29.9792345, 31.1320132], {icon: redIcon}).addTo(mymap);
+	//les jardins suspendus de Babylone
+	L.marker([48.9198611, 2.3410332], {icon: redIcon}).addTo(mymap);
+	//la statue de Zeus
+	L.marker([37.6384588, 21.6276909], {icon: redIcon}).addTo(mymap);
+	//le mausolée d'Halicarnasse
+	L.marker([37.038132, 27.4221962], {icon: redIcon}).addTo(mymap);
+	//le Temple d'Artémis
+	L.marker([37.9493601, 27.3616675], {icon: redIcon}).addTo(mymap);
+	//le Colosse de Rhodes
+	L.marker([36.4510656, 28.2236446], {icon: redIcon}).addTo(mymap);
+	//le Phare d'Alexandrie
+	L.marker([48.9671362, 2.3423655], {icon: redIcon}).addTo(mymap);
 
+	//merveilles d'afrique
 	// Mont Kilimandjaro
 	L.marker([-3.0674245, 37.3381177], {icon: greenIcon}).addTo(mymap);
 	//Le lac Retba
@@ -68,17 +72,17 @@ angular.module('mapControllers', [])
 
 	//fonction pour avoir le nom de ville avec un click & les coordonnées de l'endroit
 	var popup = L.popup();
-	function onMapClick(e) {
+	/*function onMapClick(e) {
 		popup
 			.setLatLng(e.latlng)
 			.setContent("Les coordonnées de cet endroit " + e.latlng.toString())
 			.openOn(mymap);
-		/*$http.get("https://nominatim.openstreetmap.org/reverse?format=json&lat="+e.latlng.lat+"&lon="+e.latlng.lng)
-		.success(function (response) {
-			$scope.ville=response.address.town;
+		//$http.get("https://nominatim.openstreetmap.org/reverse?format=json&lat="+e.latlng.lat+"&lon="+e.latlng.lng)
+		//.success(function (response) {
+		//	$scope.ville=response.address.town;
 			//console.log($scope.ville);
-		})	*/
-	}
+		//})	
+	}*/
 	
 	info.nbrofclick = 0;
 	info.showHide=true;
@@ -90,13 +94,18 @@ angular.module('mapControllers', [])
 		info.showHide=false;
 		$scope.ApiCall='GetQuestionsFromFile';
 	}
-
+	this.resetquestion=function resetQuestion(){
+		info.rndquestion='';
+		info.showHide=true;
+		$scope.essais=0;
+	}
+	
 	this.getrndQuestion=function Getrndquestion(){
 		info.rndquestion="";
-
+		
 		$http.get('api/'+$scope.ApiCall).success(function (response) {
 			var taille = response.maquestion.length;
-			var randomvalue = Math.floor(Math.random() * taille)
+			$scope.randomvalue = Math.floor(Math.random() * taille)
 			for(var i =0; i<taille; i++){
 				$scope.question[i]=response.maquestion[i].laquestion;
 				$scope.crdx[i]=response.maquestion[i].coordonnee_x;
@@ -106,6 +115,7 @@ angular.module('mapControllers', [])
 			
 			//conversion en base64 de l'image 
 			var imgdata =response.maquestion[0].merveille_image.data;
+			//console.log(imgdata);
 			var binary = '';
 			var bytes = new Uint8Array(imgdata);
 			var len = bytes.byteLength;
@@ -113,91 +123,98 @@ angular.module('mapControllers', [])
 				binary += String.fromCharCode( bytes[ i ] );
 			}
 			info.limage=btoa(binary);
-			console.log(info.limage);
+			//console.log(info.limage);
 			/***************************************************************/
+			var randomQuestion = $scope.question[$scope.randomvalue];
+			var randomQuestion_crdx = $scope.crdx[$scope.randomvalue];
+			var randomQuestion_crdy = $scope.crdy[$scope.randomvalue];
+			var id = $scope.ids[$scope.randomvalue];
 			
-
-
-			var randomQuestion = $scope.question[randomvalue];
-			var randomQuestion_crdx = $scope.crdx[randomvalue];
-			var randomQuestion_crdy = $scope.crdy[randomvalue];
-			var id = $scope.ids[randomvalue];
-		
-			function CalculDistance(e){
-				var score = 100;
-				//var i = 0;
-				var distance = Math.sqrt(((randomQuestion_crdx-e.latlng.lat)*(randomQuestion_crdx-e.latlng.lat))
-				+((randomQuestion_crdy-e.latlng.lng)*(randomQuestion_crdy-e.latlng.lng)));
-				console.log('Distance : '+ distance);
-				//requete pour recuperer les données json
-				if(distance <= 0.4){
-					
-					info.chance='Excellent!! votre score est '+ score +' sur 100';
-					console.log(info.chance);
-				}
-				if(distance > 0.4 && distance<=0.5){
-					score=score-5;
-					info.chance='Bien! votre score est '+ score +' sur 100, presque parfait!';
-				}
-				if(distance > 0.5 && distance<=0.6){
-					score=score-10;
-					
-					info.chance='Vous étiez proche! votre score est '+score+' sur 100, vous pouvez faire mieux';
-				}
-				if(distance>0.6 && distance <= 0.7){
-					score=score-30;
-					
-					info.chance='Vous êtes un peu loin là! votre score est '+score +' sur 100';					
-				}
-				if(distance>0.7 && distance <= 0.9){
-					score=score-80;
-					
-					info.chance='Vous êtes loin! votre score est '+score +' sur 100';					
-				}
-				if(distance>0.9){
-					score=score-100;
-					
-					info.chance='Oulala trop loin!! votre score est '+score +' sur 100';					
-				}
-				console.log(randomQuestion);
-
-				//Mettre le score dans la base de données
-				$http.get('/api/addScore/'+id).success(function (response) {
-						$scope.donnee=response;
-						$scope.donnee.les_scores=score;
-						console.log('le score dans get '+score);
-						$http.put('/api/addScore/'+id, $scope.donnee).success(function (response) {
-						console.log($scope.donnee);
-					})
-						
-				})
-					
-			}
 			info.rndquestion=randomQuestion;
 			
-			if(info.nbrofclick>2){
+
+			/*if(info.nbrofclick>2){
 				info.chance=null;
 				info.nbrofclick=1;
-			}
-
-
-			mymap.on('click', CalculDistance);
-		
+			}*/		
 			
 		})
-		info.nbrofclick++;
+		//info.nbrofclick++;
 
-		mymap.on('click', Getrndquestion);
-	}
+		//mymap.on('click', Getrndquestion);
+	}//fin getrndquestion
 	
-	this.resetquestion=function resetQuestion(){
-		info.chance=null;
-		info.rndquestion='';
-		info.nbrofclick=0;
-		info.showHide=true;
-	}
+	function CalculDistance(e){
+		var score = 100;
+		$scope.distance = Math.sqrt((($scope.crdx[$scope.randomvalue]-e.latlng.lat)*($scope.crdx[$scope.randomvalue]-e.latlng.lat))
+		+(($scope.crdy[$scope.randomvalue]-e.latlng.lng)*($scope.crdy[$scope.randomvalue]-e.latlng.lng)));
+		//console.log('Distance : '+ $scope.distance);
+		if($scope.distance <= 0.4){
+			$scope.chance='Excellent!! votre score est '+ score +' sur 100';
+			console.log($scope.chance);
+		}
+		if($scope.distance > 0.4 && $scope.distance<=0.5){
+			score=score-5;
+			$scope.chance='Bien! votre score est '+ score +' sur 100, presque parfait!';
+		}
+		if($scope.distance > 0.5 && $scope.distance<=0.6){
+			score=score-10;
+			
+			$scope.chance='Vous étiez proche! votre score est '+score+' sur 100, vous pouvez faire mieux';
+		}
+		if($scope.distance>0.6 && $scope.distance <= 0.7){
+			score=score-30;
+			
+			$scope.chance='Vous êtes un peu loin là! votre score est '+score +' sur 100';					
+		}
+		if($scope.distance>0.7 && $scope.distance <= 0.9){
+			score=score-80;
+			$scope.chance='Vous êtes loin! votre score est '+score +' sur 100';					
+		}
+		if($scope.distance>0.9){
+			score=score-100;
+			$scope.chance='Oulala trop loin!! votre score est '+score +' sur 100';
+			console.log($scope.chance);			
+		}
+		
+		if($scope.distance && $scope.chance && info.showHide==false && $scope.essais!=4){
+			popup
+			.setLatLng(e.latlng)
+			.setContent($scope.chance+' distance '+$scope.distance)
+			.openOn(mymap);
+			if(score==100 && $scope.essais<4){
+				$scope.essais=3;
+			}
+			
+			if($scope.essais==3){
+				popup
+			.setLatLng(e.latlng)
+			.setContent('Cliquez sur Rejouer pour lancer une autre partie!')
+			.openOn(mymap);
+			}
+			$scope.essais++;
+		}
+		
+		console.log(info.showHide);
+		console.log($scope.essais);
+		//console.log($scope.question[$scope.randomvalue]);
+		//Mettre le score dans la base de données
+		/*$http.get('/api/addScore/'+id).success(function (response) {
+				$scope.donnee=response;
+				$scope.donnee.les_scores=score;
+				console.log('le score dans get '+score);
+				$http.put('/api/addScore/'+id, $scope.donnee).success(function (response) {
+				console.log($scope.donnee);
+			})
+				
+		})*/
+	}//distance fonction
+
 	
-	mymap.on('click', onMapClick);
+
+	mymap.on('click', CalculDistance);
+
+	//mymap.on('click', onMapClick);
 	
 
 });
