21f6f08149af6099af59c38f1cbb60f9a352fb16
MZIRDA Zakaria <zakaria.mzirda@outlook.fr>
2018-02-25 16:41:52 +0100

    modification params

--- public/app/controllers/mapCtrl.js
+++ public/app/controllers/mapCtrl.js
@@ -170,14 +170,14 @@ angular.module('mapControllers', [])
 
 		//Calculer la distance en Km
 		var R = 6371; // rayon de la terre en km
-		var φ1 = lat1*(Math.PI/180);
-		var φ2 = lat2*(Math.PI/180);
-		var Δφ = (lat2-lat1)*(Math.PI/180);
-		var Δλ = (lon2-lon1)*(Math.PI/180);
+		var phi1 = lat1*(Math.PI/180);
+		var phi2 = lat2*(Math.PI/180);
+		var deltaPhi = (lat2-lat1)*(Math.PI/180);
+		var deltaY = (lon2-lon1)*(Math.PI/180);
 
-		var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-				Math.cos(φ1) * Math.cos(φ2) *
-				Math.sin(Δλ/2) * Math.sin(Δλ/2);
+		var a = Math.sin(deltaPhi/2) * Math.sin(deltaPhi/2) +
+				Math.cos(phi1) * Math.cos(phi2) *
+				Math.sin(deltaY/2) * Math.sin(deltaY/2);
 		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
 		var DistanceEnKM = parseInt(R * c);
